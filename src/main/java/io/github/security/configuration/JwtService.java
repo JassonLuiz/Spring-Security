acc1738f744e4c20e49dd7f42cd1f3e00caa6877
Jasson Luiz <jassonluizjr@hotmail.com>
2023-04-01 14:51:15 -0300

    Configuring the doFilterInternal and create the class JwtService

---
+++ src/main/java/io/github/security/configuration/JwtService.java
@@ -0,0 +1,11 @@
+package io.github.security.configuration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+}
