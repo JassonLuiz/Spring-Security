688ac1cab802dc400fadf49d8929b689c6c3a8d8
Jasson Luiz <jassonluizjr@hotmail.com>
2023-04-02 13:06:36 -0300

    Including the secret key and updating the class JwtService

--- src/main/java/io/github/security/configuration/JwtService.java
+++ src/main/java/io/github/security/configuration/JwtService.java
@@ -1,11 +1,37 @@
 package io.github.security.configuration;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
