4dd9c3b4191aa99994648f9937226266890643aa
Jasson Luiz <jassonluizjr@hotmail.com>
2023-04-04 21:39:34 -0300

    Updating the method doFilterInternal and creating the ApplicationConfig class

---
+++ src/main/java/io/github/security/configuration/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package io.github.security.configuration;
+
+import io.github.security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
