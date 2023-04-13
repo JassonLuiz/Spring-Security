c079134626bf92f20200137a1e5adca87c67fcdd
Jasson Luiz <jassonluizjr@hotmail.com>
2023-04-13 20:27:01 -0300

    Create the AuthenticationService class

---
+++ src/main/java/io/github/security/auth/AuthenticationService.java
@@ -0,0 +1,15 @@
+package io.github.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    public AuthenticationResponse register(RegisterRequest request) {
+        
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+    }
+}
